1f1ed936a692db5204a62d4bba920820991bdaf6
KristoferGauti <kristofergauti@gmail.com>
2022-12-04 18:16:08 +0000

    Part2 of day 1 completed

--- day1/day1.py
+++ day1/day1.py
@@ -18,17 +18,22 @@ def parse_file_content(file_content):
             sub_calorie_list.append(line)
     return [list(map(lambda x: x.strip(), calorie_list)) for calorie_list in calorie_lists]
 
-def elf_with_most_calories(cal_list):
+def get_sum_of_elf_calories_(cal_list):
     sum_list = []
     for calorie_list in cal_list:
         sum_list.append(sum(list(map(lambda x: int(x), calorie_list))))
-    return max(sum_list)
+    return sum_list
 
 
 def main():
+    # first half of the puzzle
     file_content = read_file("./day1/input.txt")
     cal_list = parse_file_content(file_content)
-    max_calorie_count = elf_with_most_calories(cal_list)
+    sum_of_elf_calories = get_sum_of_elf_calories_(cal_list)
+    max_calorie_count = max(sum_of_elf_calories)
     print(max_calorie_count)
 
+    # second half of the puzzle
+    top_three = sorted(sum_of_elf_calories, key=lambda x: int(x), reverse=True)[:3]
+    print(sum(top_three))
 main()
